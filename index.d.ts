40f3cf417af37c387efcb4c6075b7f097c22c0d4
Jon Sakas <jon.sakas@gmail.com>
2021-02-16 15:52:43 -0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,17 @@
-type StorageFallback = Pick<Storage, 'clear' | 'getItem' | 'setItem' | 'removeItem'>
-
+type StorageFallback = Pick<Storage, 'clear' | 'getItem' | 'setItem' | 'removeItem'>;
 declare module 'local-storage-fallback' {
-    const localStorageFallback: StorageFallback;
-    export default localStorageFallback;
+    const storage: Storage | StorageFallback;
+    export default storage;
+
+    export const CookieStorage: {
+        prototype: StorageFallback;
+        new(options?: object): StorageFallback;
+    };
+
+    export const MemoryStorage: {
+        prototype: StorageFallback;
+        new(): StorageFallback;
+    };
+
+    export function isSupported(name?: string): boolean;
 }
